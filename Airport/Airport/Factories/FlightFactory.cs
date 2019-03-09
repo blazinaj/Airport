5079b39becad762b765cb5145583083a4d8a755e
Anatoli Railean <anatoli.railean@gmail.com>
2019-03-08 16:48:03 -0800

    Check for past date

--- Airport/Airport/Factories/FlightFactory.cs
+++ Airport/Airport/Factories/FlightFactory.cs
@@ -19,33 +19,48 @@ namespace Airport.Factories
             List<Airport> airportList, 
             List<Airline> airlineList)
         {
-            List<Airline> airlineResults = airlineList.FindAll(x => x.AirlineName == airlineName);
-            List<Airport> airportOrigResults = airportList.FindAll(x => x.AirportName == originAirport);
-            List<Airport> airportDestResults = airportList.FindAll(x => x.AirportName == destAirport);
             string result;
 
             // Invalid Airline
-            if (airlineResults.Count < 1)
+            if (airlineList.FindAll(x => x.AirlineName == airlineName).Count < 1)
             {
                 throw new InvalidAirlineException("Error: Could not Create Flight, " + airlineName + " is not a valid airline!");
             }
+
             // Invalid Origin Airport
-            else if (airportOrigResults.Count < 1)
+            if (airportList.FindAll(x => x.AirportName == originAirport).Count < 1)
             {
                 throw new InvalidAirportException("Error: Could not Create Flight, " + originAirport + " is not a valid airport!");
             }
+            
             // Invalid Destination Airport
-            else if (airportDestResults.Count < 1)
+            if (airportList.FindAll(x => x.AirportName == destAirport).Count < 1)
             {
                 throw new InvalidAirportException("Error: Could not Create Flight, " + destAirport + " is not a valid airport!");
             }
+
+            if (year < DateTime.Now.Year)
+            {
+                if (month < DateTime.Now.Month)
+                {
+                    if (day < DateTime.Now.Day)
+                    {
+                        throw new InvalidAirportException("Error: Could not Create Flight for past day!");
+                    }
+
+                    throw new InvalidAirportException("Error: Could not Create Flight for past month!");
+                }
+
+                throw new InvalidAirportException("Error: Could not Create Flight for past year!");
+            }
+
             // Valid Airports and Airline
-            else if (airlineResults.Count > 0 && airportOrigResults.Count > 0 && airportDestResults.Count > 0)
+            if (airportList.FindAll(x => x.AirportName == originAirport).Count > 0 && airportList.FindAll(x => x.AirportName == originAirport).Count > 0 && airportList.FindAll(x => x.AirportName == destAirport).Count > 0)
             {
                 // If All List Checks Pass, call CreateFlight in Airline for other checks
                 try
                 {
-                    result = airlineResults.Find(x => x.AirlineName == airlineName).CreateFlight(airlineName, originAirport, destAirport, year, month, day, id);
+                    result = airlineList.FindAll(x => x.AirlineName == airlineName).Find(x => x.AirlineName == airlineName).CreateFlight(airlineName, originAirport, destAirport, year, month, day, id);
                 }
                 catch (InvalidFlightException e)
                 {
