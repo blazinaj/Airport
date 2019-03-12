f5053e0000f668dfe41cd7a930b43ab009d4939c
Anatoli Railean <anatoli.railean@gmail.com>
2019-03-11 17:41:38 -0700

    Test Unit Project Created

---
+++ Transport/Transport.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Transport.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
