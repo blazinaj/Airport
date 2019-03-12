07355a22c3705d24352cb8146554355e57668f7c
Anatoli Railean <anatoli.railean@gmail.com>
2019-03-12 16:36:28 -0700

    Client Menu

---
+++ Transport/Transport/Menu/CruiseMenu.cs
@@ -0,0 +1,12 @@
+﻿using Transport.Menu;
+
+namespace Transport
+{
+    internal class CruiseMenu : SystemMenu
+    {
+        public override string DisplayMenu()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Transport/Transport/Menu/TrainMenu.cs
@@ -0,0 +1,12 @@
+﻿using Transport.Menu;
+
+namespace Transport
+{
+    internal class TrainMenu : SystemMenu
+    {
+        public override string DisplayMenu()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
