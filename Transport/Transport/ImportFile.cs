1aadbd453d086716013987e203fecec78c262499
Anatoli Railean <anatoli.railean@gmail.com>
2019-03-11 18:11:06 -0700

    Start Read from input File

---
+++ Transport/Transport/ImportFile.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Transport
+{
+    public class ImportFile
+    {
+        public static string ReadFromFile(string[] args)
+        {
+            FileStream fileStream;
+
+            if (args.Length != 0)
+            {
+                string inputFileName = args[0];
+
+                // change to your path to make it work
+                fileStream =
+                    new FileStream($@"C:\Users\Anatoli\Source\Repos\Airport\Transport\Transport\{inputFileName}",
+                        FileMode.Open, FileAccess.Read);
+            }
+            else
+            {
+                // change to your path to make it work
+                fileStream =
+                    new FileStream(@"C:\Users\Anatoli\Source\Repos\Airport\Transport\Transport\inputFile.in",
+                        FileMode.Open, FileAccess.Read);
+            }
+
+            using (StreamReader streamReader = new StreamReader(fileStream, Encoding.UTF8))
+            {
+
+            }
+
+
+            return "";
+        }
+    }
+}
