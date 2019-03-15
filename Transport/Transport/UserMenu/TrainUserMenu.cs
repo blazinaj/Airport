d4ef15e602142e9989f12948d3c3eedd51ff472b
Anatoli Railean <anatoli.railean@gmail.com>
2019-03-14 19:25:08 -0700

    UserMenu Done

--- Transport/Transport/UserMenu/TrainUserMenu.cs
+++ Transport/Transport/UserMenu/TrainUserMenu.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Transport.Menu;
 
 namespace Transport.UserMenu
 {
@@ -13,18 +14,72 @@ namespace Transport.UserMenu
             Console.WriteLine("Please choose one of the following from menu:");
             Console.WriteLine();
             Console.WriteLine("0. Create an Train System from a file");
-            Console.WriteLine("1. Change the price associated with seats in a train section");
-            Console.WriteLine("2. Query the system for trips with available seats in a given class ");
+            Console.WriteLine("1. Change the price associated with seats in a TrainJourney section");
+            Console.WriteLine("2. Query the system for TrainJourney with available seats in a given class");
             Console.WriteLine("3. Change the seat class");
-            Console.WriteLine("4. Book a seat given a specific seat on a trip");
-            Console.WriteLine("5. Book a seat on a trip given only a seating preference");
+            Console.WriteLine("4. Book a seat given a specific seat on a TrainJourney");
+            Console.WriteLine("5. Book a seat on a TrainJourney given only a seating preference");
             Console.WriteLine("6. Display Train Transportation System");
             Console.WriteLine("7. Save Train Transportation System to a file");
-            Console.WriteLine("8. Go to the Main Menu");
+            Console.WriteLine("8. Go to the Admin Menu");
+            Console.WriteLine("9. Go to the Main Menu");
             var isNumeric = int.TryParse(Console.ReadLine(), out int res);
 
             if (res > -1 && res < 9)
             {
+                if (res == 0)
+                {
+                    TrainImportFile.ReadFromFile();
+                }
+
+                if (res == 1)
+                {
+                    //Change the price associated with seats in a flight section
+                }
+
+                if (res == 2)
+                {
+                    //Query the system for flights with available seats in a given class
+                }
+
+                if (res == 3)
+                {
+                    //Change the seat class
+                }
+
+                if (res == 4)
+                {
+                    //Book a seat given a specific seat on a flight
+                }
+
+                if (res == 5)
+                {
+                    //Book a seat on a flight given only a seating preference
+                }
+
+                if (res == 6)
+                {
+                    //Display Airport Transportation System
+                }
+
+                if (res == 7)
+                {
+                    //Save Airport Transportation System to a file
+                }
+
+                if (res == 8)
+                {
+                    // Go to administrator menu
+                    SystemMenu adminMenu = new TrainMenu();
+                    adminMenu.DisplayMenu();
+                }
+
+                if (res == 9)
+                {
+                    //Go to the Main Menu
+                    UserMenu userMenu = new ClientMenu();
+                    userMenu.DisplayMenu();
+                }
 
             }
 
